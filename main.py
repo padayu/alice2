7cc3536ac9872d7b2ac618143a14c3538c0b14c4
padayu <76900071+padayu@users.noreply.github.com>
2021-04-22 18:47:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -42,10 +42,10 @@ def handle_dialog(res, req):
         res['response']['text'] = 'Ты не написал название не одного города!'
     elif len(cities) == 1:
         res['response']['text'] = 'Этот город в стране - ' + \
-                                  get_country(cities[0])
+                                  get_geo_info(cities[0], 'country')
     elif len(cities) == 2:
-        distance = get_distance(get_coordinates(
-            cities[0]), get_coordinates(cities[1]))
+        distance = get_distance(get_geo_info(cities[0], 'coordinates'), 
+                                get_geo_info(cities[1], 'coordinates'))
         res['response']['text'] = 'Расстояние между этими городами: ' + \
                                   str(round(distance)) + ' км.'
     else:
